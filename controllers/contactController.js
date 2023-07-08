4646380990a6023398579b20669dd8316fb40e30
Just2Deep <deepakrajsrs@gmail.com>
2023-07-08 19:46:29 +0530

    initial commit

---
+++ controllers/contactController.js
@@ -0,0 +1,85 @@
+const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel");
+//@desc get all contacts
+//@route GET api/contacts
+//@access Public
+
+const getContacts = asyncHandler(async (req, res) => {
+  const contact = await Contact.find();
+  res.status(200).json(contact);
+});
+
+//@desc post a contact
+//@route POST api/contacts
+//@access Public
+
+const createContact = asyncHandler(async (req, res) => {
+  const { name, email, phone } = req.body;
+
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
+  const contact = await Contact.create({
+    name,
+    email,
+    phone,
+  });
+  res.status(201).json(contact);
+});
+
+//@desc get a contact
+//@route GET api/contacts/:id
+//@access Public
+
+const getContact = asyncHandler(async (req, res) => {
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact Not found");
+  }
+
+  res.status(200).json(contact);
+});
+
+//@desc update a contact
+//@route PUT api/contacts/:id
+//@access Public
+
+const updateContact = asyncHandler(async (req, res) => {
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact Not found");
+  }
+
+  const updatedContact = await Contact.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  res.status(200).json(updatedContact);
+});
+
+//@desc delete a contact
+//@route DELETE api/contacts/:id
+//@access Public
+
+const deleteContact = asyncHandler(async (req, res) => {
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact Not found");
+  }
+  await contact.deleteOne();
+  res.status(200).json(contact);
+});
+
+module.exports = {
+  getContacts,
+  createContact,
+  getContact,
+  updateContact,
+  deleteContact,
+};
